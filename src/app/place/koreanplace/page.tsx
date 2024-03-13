f2a80112c5bcfe81b036d3eca1be0bb4b4ca6e9a
computerkingminsu <ya5656@naver.com>
2024-03-14 00:18:19 +0900

    feat: schooleats next14 migration

--- src/app/place/koreanplace/page.tsx
+++ src/app/place/koreanplace/page.tsx
@@ -2,13 +2,14 @@
 
 import * as S from '../../../components/unit/place/Place.styles';
 import React, { Suspense } from 'react';
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import KoreanPlace from '@/components/unit/place/KoreanPlace/koreanPlace.index';
 
 // const KoreanPlace = dynamic(() => import('../../../components/unit/place/KoreanPlace/koreanPlace.index'), {
 //   suspense: true,
 // });
+
+export const dynamic = 'force-dynamic';
 
 export default function koreanplace() {
   return <KoreanPlace />;
@@ -17,29 +18,6 @@ export default function koreanplace() {
   //   <Suspense
   //     fallback={
   //       <>
-  //         <S.Wrapper>
-  //           <S.Title>테마별 맛집🍚</S.Title>
-  //           <S.SubTitle>스쿨잇츠가 소개하는 맛집 리스트!</S.SubTitle>
-  //           <S.ButtonWrapper>
-  //             <S.SelectButton>ALL</S.SelectButton>
-
-  //             <Link href="/place/koreanplace">
-  //               <S.Button>한식</S.Button>
-  //             </Link>
-  //             <Link href="/place/chineseplace">
-  //               <S.Button>중식</S.Button>
-  //             </Link>
-  //             <Link href="/place/japaneseplace">
-  //               <S.Button>일식</S.Button>
-  //             </Link>
-  //             <Link href="/place/westernplace">
-  //               <S.Button>양식</S.Button>
-  //             </Link>
-  //             <Link href="/place/cafeplace">
-  //               <S.Button>카페</S.Button>
-  //             </Link>
-  //           </S.ButtonWrapper>
-  //         </S.Wrapper>
   //       </>
   //     }
   //   >
