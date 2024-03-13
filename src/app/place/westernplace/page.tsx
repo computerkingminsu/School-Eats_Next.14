80d330ed7acb2593cded54c3d259db0dd260244a
computerkingminsu <ya5656@naver.com>
2024-03-14 00:37:19 +0900

    feat: schooleats next14 migration

--- src/app/place/westernplace/page.tsx
+++ src/app/place/westernplace/page.tsx
@@ -3,18 +3,19 @@
 import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
-// import WesternPlace from '@/components/unit/place/WesternPlace/WesternPlace.index';
+import WesternPlace from '@/components/unit/place/WesternPlace/WesternPlace.index';
 
-const WesternPlace = dynamic(() => import('../../../components/unit/place/WesternPlace/WesternPlace.index'), {
-  suspense: true,
-});
+// const WesternPlace = dynamic(() => import('../../../components/unit/place/WesternPlace/WesternPlace.index'), {
+//   suspense: true,
+// });
 
 export default function westernplace() {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <WesternPlace />
-      </Suspense>
-    </div>
-  );
+  return <WesternPlace />;
+  // return (
+  //   <div>
+  //     <Suspense fallback={<div>Loading...</div>}>
+  //       <WesternPlace />
+  //     </Suspense>
+  //   </div>
+  // );
 }
